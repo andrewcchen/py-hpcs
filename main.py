59646ae4347ce984a2236af8b758b6ee5618b35a
Andrew Chen <andrew@xortux.com>
2025-12-02 22:48:13 +0000

    improvements

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# dependencies = [
+#   "bleak==2.*",
+# ]
+# ///
+
+import argparse
+import collections
 from dataclasses import dataclass
 from enum import Enum, auto
 from pprint import pprint
@@ -19,10 +29,11 @@ class DeviceInfo:
     status: int = 0
 
 
-class SM:
+class SM(Enum):
     Idle = auto()
     GetDeviceInfo = auto()
     StartSamping = auto()
+    ReadIntegTime = auto()
     ReadSampleState = auto()
     ReadTestResult = auto()
     Stop = auto()
@@ -33,11 +44,13 @@ class State:
     client: bleak.BleakClient
     state_machine: SM
     device_info: DeviceInfo
-    response: bytearray
+    integration_time: float
+    response_data: bytearray
     response_length: int
     response_future: asyncio.Future
-
-
+    header_printed: bool
+    def __init__(self):
+        self.header_printed = False
 s = State()
 
 
@@ -46,63 +59,71 @@ async def notify_handler(sender: bleak.BleakGATTCharacteristic, data: bytearray)
 
         case SM.GetDeviceInfo:
             assert data[:2] == bytes.fromhex("8CEE")
-            i = DeviceInfo()
-            i.version = int.from_bytes(data[16:18], "little")
-            i.name = data[2:12].decode("ascii", errors="ignore").rstrip("\x00")
-            i.serial = int.from_bytes(data[12:16], "little")
-            i.battery = int(data[18])
-            i.status = int(data[19])
-            s.device_info = i
+            info = DeviceInfo()
+            info.version = int.from_bytes(data[16:18], "little")
+            info.name = data[2:12].decode("ascii", errors="ignore").rstrip("\x00")
+            info.serial = int.from_bytes(data[12:16], "little")
+            info.battery = int(data[18])
+            info.status = int(data[19])
             s.state_machine = SM.Idle
-            s.response_future.set_result(True)
+            s.response_future.set_result(info)
 
         case SM.StartSamping:
             assert data[:2] == bytes.fromhex("8C0E")
             s.state_machine = SM.Idle
-            s.response_future.set_result(True)
+            s.response_future.set_result(None)
+
+        case SM.ReadIntegTime:
+            assert data[:2] == bytes.fromhex("8C05")
+            s.state_machine = SM.Idle
+            time = int.from_bytes(data[2:6], "little") / 1e3
+            s.response_future.set_result(time)
 
         case SM.ReadSampleState:
             assert data[:2] == bytes.fromhex("8C03")
             assert len(data) >= 9
-            if data[3] != 1:
-                await asyncio.sleep(0.1)
-                await send_command(SM.ReadSampleState, "8C03")
-            else:
-                s.state_machine = SM.Idle
-                s.response_future.set_result(True)
+            s.state_machine = SM.Idle
+            s.response_future.set_result(data[3] == 1)
 
         case SM.ReadTestResult:
-            if not s.response:
+            if not s.response_data:
                 assert data[:2] == bytes.fromhex("8C13")
-                s.response = data.copy()
+                s.response_data = data.copy()
                 s.response_length = int.from_bytes(data[2:4], "big") + 4
             else:
-                s.response += data
-            if len(s.response) >= s.response_length:
-                whole_response = s.response.copy()
-                s.response.clear()
+                s.response_data += data
+            # print(f'got {len(data)} total {len(s.response_data)} expect {s.response_length}', file=sys.stderr)
+            if len(s.response_data) >= s.response_length:
+                whole_response = s.response_data.copy()
+                s.response_data.clear()
                 s.response_length = 0
                 s.state_machine = SM.Idle
                 s.response_future.set_result(whole_response)
 
         case SM.Stop:
             s.state_machine = SM.Idle
-            s.response_future.set_result(True)
+            s.response_future.set_result(None)
 
         case _:
             assert False
 
 
 
-async def send_command(sm: SM, hex: str):
+async def command(sm: SM, hex: str, timeout=1):
     s.state_machine = sm
     s.response_future = s.loop.create_future()
     await s.client.write_gatt_char(CHAR_UUID, bytes.fromhex(hex), response=False)
+    return await asyncio.wait_for(s.response_future, timeout)
 
 
+def process_result(data: bytearray) -> tuple[dict[str, float], list[float]]:
+    name = s.device_info.name
+    iver = s.device_info.version
 
-def process_result(data: bytearray):
-    field_names = "fLx fEfc cct duv x y u v u2 v2 fSDCM Ra R1 R2 R3 R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 fSP DominantWave Purity HalfWidth PeakWave CentreWave CentroidWave fRratio fGratio fBratio fEML fEeml fEmlRatio fEDI_lx IntegTime0 VPeak VDark VDarkDAC"
+    if (name.endswith('310') or name.endswith('330')) and iver > 2005:
+        field_names = "fLx fEfc cct duv x y u v u2 v2 fSDCM Ra R1 R2 R3 R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 fSP DominantWave Purity HalfWidth PeakWave CentreWave CentroidWave fRratio fGratio fBratio fEML fEeml fEmlRatio fEDI_lx IntegTime0 VPeak VDark VDarkDAC"
+    else:
+        raise RuntimeError("TODO add support for device")
     field_list = [name.strip() for name in field_names.split(' ') if name.strip()]
 
     offset = 40
@@ -110,7 +131,7 @@ def process_result(data: bytearray):
     field_count = len(field_list)
     field_bytes = 4 * field_count
     field_values = struct.unpack(f"<{field_count}f", data[offset:offset+field_bytes])
-    processed = dict(zip(field_list, field_values))
+    fields = collections.OrderedDict(zip(field_list, field_values))
 
     offset += field_bytes
     offset += 20
@@ -118,23 +139,66 @@ def process_result(data: bytearray):
     spectrum_length = 671
     spectrum_bytes = 4 * spectrum_length
     spectrum_chunk = data[offset:offset+spectrum_bytes]
-    spectrum = list(struct.unpack(f"<{spectrum_length}f", spectrum_chunk))
+    spectrum = [v * 0.1 for v in struct.unpack(f"<{spectrum_length}f", spectrum_chunk)]
 
     start_test_wave, end_test_wave = struct.unpack("<2f", data[-8:])
-    processed["StartTestWave"] = start_test_wave
-    processed["EndTestWave"] = end_test_wave
-
-    processed["spectrum"] = [v * 0.1 for v in spectrum]
+    fields["StartTestWave"] = int(start_test_wave)
+    fields["EndTestWave"] = int(end_test_wave)
 
     assert offset + spectrum_bytes == len(data) - 8
 
-    return processed
+    return fields, spectrum
 
 
-async def main():
+def print_result(processed: tuple[dict[str, float], list[float]]):
+    fields, spectrum = processed
+    if not s.header_printed:
+        for name in fields:
+            print(name, end=',')
+        for i in range(fields["StartTestWave"], fields["EndTestWave"] + 1):
+            print(i, end='nm,')
+        print()
+        s.header_printed = True
+    for value in fields.values():
+        print(value, end=',')
+    for i in range(fields["EndTestWave"] - fields["StartTestWave"] + 1):
+        print(spectrum[i], end=',')
+    print()
+
+
+async def capture_sample():
+    await command(SM.StartSamping, "8C0E01")
+
+    wait_time = s.integration_time + 500 if s.integration_time > 500 else 1000
+    await asyncio.sleep(wait_time / 1e3)
+    for _ in range(50):
+        try:
+            if await command(SM.ReadSampleState, "8C03", 0.1):
+                break
+            await asyncio.sleep(0.1)
+        except TimeoutError:
+            pass
+    else:
+        raise TimeoutError
+
+    s.response_data.clear()
+    try:
+        raw_result = await command(SM.ReadTestResult, "8C1331", 3)
+    except TimeoutError:
+        print("Failed to read test result", file=sys.stderr)
+        return False
+    result = process_result(raw_result)
+    print_result(result)
+
+    s.integration_time = result[0]["IntegTime0"]
+    return True
+
+
+async def main(continuous: bool):
     s.loop = asyncio.get_event_loop()
-    s.response = bytearray()
+    s.response_data = bytearray()
 
+    print("Searching for HPCS devices", file=sys.stderr)
     device = await bleak.BleakScanner.find_device_by_filter(
         lambda d, ad: d.name and d.name.startswith("HPCS")
     )
@@ -146,32 +210,32 @@ async def main():
     async with bleak.BleakClient(device) as client:
         s.client = client
         print(f"Connected to {device.name}", file=sys.stderr)
+        await asyncio.sleep(1)
 
         s.state_machine = SM.Idle
         await client.start_notify(CHAR_UUID, notify_handler)
+        await asyncio.sleep(1)
 
-        await asyncio.sleep(2)
-
-        await send_command(SM.GetDeviceInfo, "8CEE")
-        await asyncio.wait_for(s.response_future, 1)
+        s.device_info = await command(SM.GetDeviceInfo, "8CEE")
         print(s.device_info, file=sys.stderr)
 
-        await asyncio.sleep(0.1)
-        await send_command(SM.StartSamping, "8C0E01")
-        await asyncio.wait_for(s.response_future, 1)
+        s.integration_time = await command(SM.ReadIntegTime, "8C05")
+        # print(f'integration time estimate: {s.integration_time}')
 
-        await asyncio.sleep(2)
-        await send_command(SM.ReadSampleState, "8C03")
-        await asyncio.wait_for(s.response_future, 60)
+        while True:
+            got = await capture_sample()
+            if got and not continuous:
+                break
 
-        await send_command(SM.ReadTestResult, "8C1331")
-        result = await asyncio.wait_for(s.response_future, 10)
-        procesed  = process_result(result)
-        pprint(procesed, sort_dicts=False)
+        await command(SM.Stop, "8C25")
 
-        await send_command(SM.Stop, "8C25")
-        await asyncio.wait_for(s.response_future, 1)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Capture spectrum from HPCS-310 and HPCS-330")
+    parser.add_argument("-c", "--continuous", action="store_true", help="Continue sampling until interrupted")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    args = parse_args()
+    asyncio.run(main(args.continuous))
